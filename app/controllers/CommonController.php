e682176048dfb3c617f5841dd580d8570e102fe4
sify21 <sify21@163.com>
2015-07-08 10:33:45 +0800

    sify

--- app/controllers/CommonController.php
+++ app/controllers/CommonController.php
@@ -29,20 +29,18 @@ class CommonController extends Base{
         {
             try
             {
-                if(isset($info->subject)) $email->suject = $info->subject;
-                if(isset($info->body)) $email->body = $info->body;
+                if(isset($info->subject)) $email->suject = base64_encode( $info->subject );
+                if(isset($info->body)) $email->body = base64_encode( $info->body );
                 if(isset($info->fromAddress)) $email->fromAddress = $info->fromAddress;
                 if(isset($info->receiveDate)) $email->receiveDate = $info->receiveDate;
-                if(isset($info->isAnswered)) $email->isAnswered = $info->isAnswered;
-                if(isset($info->isSeen)) $email->isSeen = $info->isSeen;
-                if(isset($info->dispatcher_id)) $email->dispatcher_id = $info->dispatcher_id;
-                if(isset($info->isDispatched)) $email->isDispatched = $info->isDispatched;
-                if(isset($info->handler_id)) $email->handler_id = $info->handler_id;
-                if(isset($info->isHandled)) $email->isHandled = $info->isHandled;
                 if(isset($info->tags)) $email->tags = $info->tags;
+                if(isset($info->status)) $email->status = $info->status;
+                if(isset($info->dispatcher_id)) $email->dispatcher_id = $info->dispatcher_id;
+                if(isset($info->handler_id)) $email->handler_id = $info->handler_id;
                 $email->save();
-                $this->response->setJsonContent(['id' => $email->id, 'subject' => $email->subject, 'body' => $email->body, 'fromAddress' => $email->fromAddress, 'receiveDate' => $email->receiveDate,
-                                                'isAnswered' => $email->isAnswered, 'isSeen' => $email->isSeen, 'dispatcher_id' => $email->dispatcher_id, 'isDispatched' => $email->isDispatched, 'handler_id' => $email->handler_id, 'isHandled' => $email->isHandled, 'tags' => $email->tags]);
+                $this->response->setJsonContent(['id' => $email->id, 'subject' => $email->subject, 'body' => $email->body,
+                    'fromAddress' => $email->fromAddress, 'receiveDate' => $email->receiveDate, 'tags' => $email->tags,
+                    'status' => $email->status, 'dispatcher_id' => $email->dispatcher_id, 'handler_id' => $email->handler_id] );
             }
             catch(Exception $e)
             {
@@ -78,17 +76,19 @@ class CommonController extends Base{
         {
             try
             {
-                if(isset($info->subject)) $email->suject = $info->subject;
-                if(isset($info->body)) $email->body = $info->body;
+                if(isset($info->subject)) $email->suject = base64_encode( $info->subject );
+                if(isset($info->body)) $email->body = base64_encode( $info->body );
+                if(isset($info->reply_id)) $email->reply_id = $info->reply_id;
                 if(isset($info->toWhom)) $email->toWhom = $info->toWhom;
                 if(isset($info->replyDate)) $email->replyDate = $info->replyDate;
+                if(isset($info->assessor_advice)) $email->assessor_advice = base64_encode( $info->assessor_advice );
+                if(isset($info->status)) $email->status = $info->status;
                 if(isset($info->handler_id)) $email->handler_id = $info->handler_id;
-                if(isset($info->isSend)) $email->isSend = $info->isSend;
                 if(isset($info->assessor_id)) $email->assessor_id = $info->assessor_id;
-                if(isset($info->isAssessed)) $email->isAssessed = $info->isAssessed;
-                if(isset($info->assessor_advice)) $email->assessor_advice = $info->assessor_advice;
                 $email->save();
-                $this->response->setJsonContent(['message' => 'success']);
+                $this->response->setJsonContent(['id' => $email->id, 'subject' => $email->subject, 'body' => $email->body, 'reply_id' => $email->reply_id,
+                    'toWhom' => $email->toWhom, 'replyDate' => $email->replyDate, 'assessor_advice' => $email->assessor_advice, 'tags' => $email->tags,
+                    'status' => $email->status, 'dispatcher_id' => $email->dispatcher_id, 'handler_id' => $email->handler_id] );
             }
             catch(Exception $e)
             {
@@ -100,37 +100,67 @@ class CommonController extends Base{
     }
 
     /**
-     * @Route("/getEmail", methods = {"GET", "OPTIONS"})
+     * @Route("/getReceiveEmail", methods = {"GET", "OPTIONS"})
      */
-    public function GetEmailAction()
+    public function GetReceiveEmailAction()
     {
         $id = $this->request->get('id');
-        $mail = ReceiveMail::findFirst([
+        $email = ReceiveMail::findFirst([
             'conditions' => 'id=?1',
             'bind' => [1 => $id]
         ]);
-        if($mail == null)
+        if($email == null)
         {
             $this->response->setJsonContent(['message' => '邮件不存在']);
         }
         else
         {
-            $id = $mail->id;
-            $mail_id = base64_decode( $mail->mail_id );
-            $subject = base64_decode( $mail->subject );
-            $body = base64_decode( $mail->body );
-            $fromAddress = $mail->fromAddress;
-            $receiveDate = $mail->receiveDate;
-            $isAnswered = $mail->isAnswered;
-            $isSeen = $mail->isSeen;
-            $dispatcher_id = $mail->dispatcher_id;
-            $isDispatched = $mail->isDispatched;
-            $handler_id = $mail->handler_id;
-            $isHandled = $mail->isHandled;
-            $tags = $mail->tags;
+            $id = $email->id;
+            $mail_id = base64_decode( $email->mail_id );
+            $subject = base64_decode( $email->subject );
+            $body = base64_decode( $email->body );
+            $fromAddress = $email->fromAddress;
+            $receiveDate = $email->receiveDate;
+            $tags = $email->tags;
+            $status = $email->status;
+            $dispatcher_id = $email->dispatcher_id;
+            $handler_id = $email->handler_id;
             $this->response->setJsonContent(['id' => $id, 'mail_id' => $mail_id, 'subject' => $subject, 'body' => $body, 'fromAddress' => $fromAddress,
-                'receiveDate' => $receiveDate, 'isAnswered' => $isAnswered, 'isSeen' => $isSeen, 'dispatcher_id' => $dispatcher_id,
-                'isDispatched' => $isDispatched, 'handler_id' => $handler_id, 'isHandled' => $isHandled, 'tags' => $tags]);
+                'receiveDate' => $receiveDate, 'tags' => $tags, 'status' => $status, 'dispatcher_id' => $dispatcher_id, 'handler_id' => $handler_id]);
+        }
+        $this->response->send();
+        return;
+    }
+
+    /**
+     * @Route("/getReplyEmail", methods = {"GET", "OPTIONS"})
+     */
+    public function GetReplyEmailAction()
+    {
+        $id = $this->request->get('id');
+        $email = ReplyMail::findFirst([
+            'conditions' => 'id=?1',
+            'bind' => [1 => $id]
+        ]);
+        if($email == null)
+        {
+            $this->response->setJsonContent(['message' => '邮件不存在']);
+        }
+        else
+        {
+            $id = $email->id;
+            $mail_id = base64_decode( $email->mail_id );
+            $subject = base64_decode( $email->subject );
+            $body = base64_decode( $email->body );
+            $reply_id = $email->reply_id;
+            $toWhom = $email->toWhom;
+            $replyDate = $email->replyDate;
+            $assessor_advice = $email->assessor_advice;
+            $status = $email->status;
+            $handler_id = $email->handler_id;
+            $assessor_id = $email->assessor_id;
+            $this->response->setJsonContent(['id' => $id, 'mail_id' => $mail_id, 'subject' => $subject, 'body' => $body, 'reply_id' => $reply_id,'toWhom' => $toWhom,
+                'replyDate' => $replyDate, 'assessor_advice' => $assessor_advice, 'status' => $status, 'handler_id' => $handler_id, 'assessor_id' => $assessor_id]);
         }
         $this->response->send();
         return;
