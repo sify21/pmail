e11a277c0ead91b759688118dc21b857bc3c2bbc
sify21 <sify21@163.com>
2015-07-09 16:28:10 +0800

    sify

---
+++ app/models/Template.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sify
+ * Date: 15-7-9
+ * Time: 下午4:26
+ */
+
+class Template extends Phalcon\Mvc\Model
+{
+    public $id;
+    public $name;
+    public $handler_id;
+    public $content;
+    public function getSource()
+    {
+        return 'template';
+    }
+    /*public function initialize()
+    {
+        $this->belongsTo('mail_id', 'ReceiveMail', 'mail_id');
+    }*/
+}
